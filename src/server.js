f822071f69bf8fa9b2e156b3cd81e380a4fc830e
Muhammad Hendra Pranajaya <mhpranajaya@gmail.com>
2021-06-03 21:59:10 +0700

    change private IP address

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.36.19',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.87.146',
     routes: {
       cors: {
         origin: ['*'],
